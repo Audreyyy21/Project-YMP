d4e0784099aa071c3ee97fa699c49180d36723b6
Audreyyy21 <audrey.10122283@mahasiswa.unikom.ac.id>
2025-10-15 23:01:31 +0700

    Upload backend Laravel project

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="container d-flex justify-content-between align-items-center">
 
     <!-- Logo -->
-    <a class="navbar-brand d-flex align-items-center" href="#">
+    <a class="navbar-brand d-flex align-items-center" href="{{ route('landing') }}">
       <img src="{{ asset('images/logoymp.png') }}" alt="Logo" style="height: 50px; object-fit: contain; width: 100px">
     </a>
 
@@ -10,24 +10,24 @@
     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item mx-3">
-          <a class="nav-link fw-semibold text-white active" href="#beranda">Beranda</a>
+          <a class="nav-link fw-semibold text-white active" href="{{ route('landing') }}#beranda">Beranda</a>
         </li>
         <li class="nav-item mx-3">
           <a class="nav-link fw-semibold text-white" href="{{ route('aboutUs') }}">Tentang Kami</a>
         </li>
         <li class="nav-item mx-3">
-          <a class="nav-link fw-semibold text-white" href="#program">Program</a>
+          <a class="nav-link fw-semibold text-white" href="{{ route('landing') }}#program">Program</a>
         </li>
-          <li class="nav-item mx-3">
-          <a class="nav-link fw-semibold text-white" href="#kontak">Kontak</a>
+        <li class="nav-item mx-3">
+          <a class="nav-link fw-semibold text-white" href="{{ route('landing') }}#kontak">Kontak</a>
         </li>
       </ul>
     </div>
 
     <!-- Tombol kanan -->
     <div class="d-flex">
-      <a href="/register" class="btn btn-outline-light rounded-pill me-2 px-4">Daftar</a>
-      <a href="/login" class="btn btn-outline-light rounded-pill px-4">Masuk</a>
+      <a href="{{ route('register.form') }}" class="btn btn-outline-light rounded-pill me-2 px-4">Daftar</a>
+      <a href="{{ route('login') }}" class="btn btn-outline-light rounded-pill px-4">Masuk</a>
     </div>
 
     <!-- Button responsive -->
